38545e325ebff65f45e37304f8578d282522530f
harvestnote <harvestnote@yandex.com>
2019-11-07 10:44:47 +0800

    20191107

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import re
 import os
-
+import wget
 
 def get_url(url):
     pattern = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+.jpg")
@@ -17,12 +17,9 @@ def get_url(url):
         if test_url.status_code == 200:
             url_data.append(url)
 
-    print(len(get_url_result))
 
-    print(len(url_data))
     return url_data
 
 
 if __name__ == "__main__":
-    get_url("http://www.520mojing.com")
     
